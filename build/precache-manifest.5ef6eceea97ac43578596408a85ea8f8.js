1edbaf71d786894e69cfd68f9d597af223d79251
Mahesh Tammineni <maheshtammineni2222@gmail.com>
2023-06-08 18:29:51 +0530

    made changes

---
+++ build/precache-manifest.5ef6eceea97ac43578596408a85ea8f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "727a4b7e5566778d875e",
+    "url": "/static/css/main.b79d9ddd.chunk.css"
+  },
+  {
+    "revision": "727a4b7e5566778d875e",
+    "url": "/static/js/main.835fc7d5.chunk.js"
+  },
+  {
+    "revision": "6a81064e3b4189a31fd1",
+    "url": "/static/js/runtime~main.e6002803.js"
+  },
+  {
+    "revision": "5dc5f07f0af4640ff77f",
+    "url": "/static/css/2.f12bf9a7.chunk.css"
+  },
+  {
+    "revision": "5dc5f07f0af4640ff77f",
+    "url": "/static/js/2.ce3f4e41.chunk.js"
+  },
+  {
+    "revision": "d06d8fe717dc215d2ddf",
+    "url": "/static/js/3.71e6714e.chunk.js"
+  },
+  {
+    "revision": "e1057ee47d3e873b8ec57427a2821f65",
+    "url": "/static/media/Web-designer-1024x821.e1057ee4.webp"
+  },
+  {
+    "revision": "c2c03bb282730d7b596bb51d89563d7d",
+    "url": "/static/media/web-removebg-preview.c2c03bb2.png"
+  },
+  {
+    "revision": "c1b7dd364005660683a50061b06cfe36",
+    "url": "/static/media/React-App-netflix-clone.c1b7dd36.png"
+  },
+  {
+    "revision": "4a960ad36860f6eb02a0bf65ada357fb",
+    "url": "/static/media/Gridlinesbuilders.4a960ad3.png"
+  },
+  {
+    "revision": "52ce0d8f480aa218c9f7329dac91eef5",
+    "url": "/static/media/Charlestown-Pizza.52ce0d8f.png"
+  },
+  {
+    "revision": "4c6cd4fdb61dd91101177178be375156",
+    "url": "/static/media/todolist.4c6cd4fd.png"
+  },
+  {
+    "revision": "41c255eabbe2f6dc68e55af65a333c08",
+    "url": "/static/media/bars.41c255ea.svg"
+  },
+  {
+    "revision": "ab217eb7a6123e19b5015acab4ba2b12",
+    "url": "/index.html"
+  }
+];
